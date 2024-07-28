01c0ed71099d7eaf29c8ec1f04fc91832a7eaf6c
Vladimir Ivanushkin <Svixdream@gmail.com>
2024-07-28 18:28:22 +0300

    complete task-1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,24 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = false // статус администратора
+const isVerifiedUser = true // подтверждённый юзер
+const hasSpecialPermission = true // спец. разрешение
+const hasTemporaryPass = false // временное разрешение
 
-let isAccess
+// Определяем, может ли пользователь получить доступ
+let isAccess = (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)
+// Проверяет, является ли пользователь администратором (isAdmin) или подтверждённым пользователем (isVerifiedUser). Если хотя бы одно из этих условий истинно, то выражение (isAdmin || isVerifiedUser) будет истинным
+
+// Проверяет, имеет ли пользователь специальное разрешение (hasSpecialPermission) или временный пропуск (hasTemporaryPass). Если хотя бы одно из этих условий истинно, то выражение (hasSpecialPermission || hasTemporaryPass) будет истинным
 
 // your code
+
+switch (isAccess) {
+    case true:
+        alert("Пользователь вошел в систему."); // Если значение isAccess равно true, выполняется первый блок case true, который выводит "Пользователь вошел в систему."
+        break;
+    case false:
+        alert("Пользователь не вошел в систему."); // Если значение isAccess равно false, выполняется второй блок case false, который выводит "Пользователь не вошел в систему."
+        
+}
