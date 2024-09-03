bf5290664ea09b9d10c1c4ccee3c14633a0531a9
Vladimir <svixdream@gmail.com>
2024-09-03 19:31:27 +0300

    fix: troubleshooting-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -3,7 +3,7 @@
 function calculateAverage(num1, num2) {
 const number_1 = 54
 const number_2 = 13
-    average = (number_1 + number_2) / 2
+    let average = (number_1 + number_2) / 2
     return average
 }
 calculateAverage()
