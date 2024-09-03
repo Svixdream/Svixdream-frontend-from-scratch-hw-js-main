21d96387789acb81c41ab3803d81e6f2afa9836d
Vladimir <svixdream@gmail.com>
2024-09-03 20:31:59 +0300

    fix: troubleshooting-3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,9 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
 function calculateAverage(num1, num2) {
-const number_1 = 10
-const number_2 = 20
-    let average = (number_1 + number_2) / 2
+    let average = (num1 + num2) / 2
     return average
 }
-calculateAverage()
+calculateAverage(10, 20)
