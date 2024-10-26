71e39e428dbee318c17fa27dbf4a4193543e180b
svixdream <svixdream@gmail.com>
2024-10-26 11:53:55 +0300

    fix: troubleshooting-2

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -10,6 +10,6 @@ function capitalizeWords(str) {
   for (let i = 0; i < words.length; i++) {
     words[i] = words[i][0].toUpperCase() + words[i].slice(1);
   }
-  return `"${words.join(" ")}"`
+  return words.join(" ")
 }
 console.log(capitalizeWords("hello world from javascript"));
