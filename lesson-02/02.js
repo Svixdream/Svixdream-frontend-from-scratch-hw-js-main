607dfd6181f3b96ef4eb6885d94198b8c6416400
Svixdream <svixdream@gmail.com>
2024-11-20 20:28:38 +0300

    feat: re-solving task-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,15 +7,11 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 6) + 1 // your code
-let dice2 = Math.floor(Math.random() * 6) + 1 // your code
-let isWinningDouble = (dice1 === dice2 && dice1 > 3) // your code
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
 
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
-} else {
-  console.log('Не выигрышный дубль.')
-}
+let isWinningDouble = (dice1 === dice2) && (dice2 > 3)
 
+console.log(`Бросаем 1 кубик и получаем ${dice1}`);
+console.log(`Бросаем 2 кубик и получаем ${dice2}`);
+console.log(`Выигрышный дубль: ${isWinningDouble}`);
