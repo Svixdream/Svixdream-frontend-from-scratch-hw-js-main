359195f8b3afe220042aa8a5855b79fc84926762
Vladimir Ivanushkin <Svixdream@gmail.com>
2024-12-10 19:37:24 +0300

    feat: re-solving task-3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,5 +1,7 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(num1, num2, num3) {
-    
+    return Math.max(num1, num2, num3); 
 }
+console.log(findLargest(110, 23, 5));
+
