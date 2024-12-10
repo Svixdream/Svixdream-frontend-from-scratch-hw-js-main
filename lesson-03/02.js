dcc52dc08470a4ffb645fa2c06837623528b5aea
Vladimir Ivanushkin <Svixdream@gmail.com>
2024-12-10 19:29:35 +0300

    feat: re-solving task-2

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,5 +2,11 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(num) {
-    
+    if (num % 2 === 0) {
+        return true
+    } else {
+        return false
+    }
 }
+console.log(isEven(43));
+
